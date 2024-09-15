7ee820ec903f6c1462e71c2455d9f72126e82797
Hryharovich Daniil <drink.wtoroy@gmail.com>
2024-09-15 19:15:58 +0300

    Created README.md, modified app_01.py, created .gitignore

--- app_01.py
+++ app_01.py
@@ -1,20 +1,20 @@
-from flask import Flask
-from models_02 import db, User, Post, Comment
+from flask import Flask, render_template
+# from models_02 import db, User, Post, Comment
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
-db.init_app(app)
+# db.init_app(app)
 
 
 @app.route('/')
 def index():
-    return 'Hi!'
+    return render_template("index.html")
 
 
 @app.cli.command("init-db")
 def init_db():
     # показать ошибку с неверным wsgi.py
-    db.create_all()
+    # db.create_all()
     print('OK')
 
 
