57c9667839dbb8948df6d97c12f78d213bd65e71
Hryharovich Daniil <drink.wtoroy@gmail.com>
2024-09-29 13:44:21 +0300

    redis fixed

--- app_01.py
+++ app_01.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from redis_client import redis_backup, add_to_cart as redis_add_to_cart, get_from_cart as redis_get_from_cart
+from redis_client import redis_backup, redis_add_to_cart,  redis_get_from_cart, redis_clear_cart
 import json
 
 # from models_02 import db, User, Post, Comment
@@ -16,18 +16,34 @@ def index():
 
 @app.route('/add_to_cart/<int:position_id>')
 def add_to_cart(position_id):
-    redis_add_to_cart(user_id=None, position_id=position_id)  # user_id надо сделать потом
+    try:
+        redis_add_to_cart(user_id=123, position_id=position_id)  # user_id надо сделать потом
+    except Exception as e:
+        return {"status": 400, "exception": e}
+    return {"status": 200}
 
 
-@app.route("/get_from_cart/")
+@app.route("/get_cart/")
 def get_from_cart():
-    positions = redis_get_from_cart(user_id=None)  # user_id надо сделать потом
+    try:
+        positions = redis_get_from_cart(user_id=123)  # user_id надо сделать потом
+        print(positions)
+        # Преобразовать байтовые строки в строки
+        positions = [pos.decode('utf-8') for pos in positions]
+        json_dict = {(i + 1): value for i, value in enumerate(positions)}
 
-    json_dict = {{i + 1}: value for i, value in enumerate(positions)}
+        if json_dict:
+            json_string = json.dumps(json_dict)
+            return json_string
 
-    json_string = json.dumps(json_dict)
-    return json_string
+    except Exception as e:
+        return {"status": 200, "content": "Cart is empty :(", "error": str(e)}
 
+
+@app.route("/clear_cart/<int:user_id>")
+def clear_cart(user_id):
+    redis_clear_cart(user_id)
+    return {"status": 200}
 
 @app.cli.command("init-db")
 def init_db():
--- redis_client.py
+++ redis_client.py
@@ -23,15 +23,35 @@ def redis_backup():
         time.sleep(1)
 
 
-def add_to_cart(user_id, position_id):
-    client.set(user_id, position_id)
-
-
-def get_from_cart(user_id):
+def redis_add_to_cart(user_id, position_id):
     if client.exists(user_id):
-        values = client.smembers(user_id)
-
-        return [value.decode('utf-8') for value in values]
-
+        key_type = client.type(user_id)
+        if key_type == b'list':
+            client.rpush(user_id, position_id)
+        elif key_type == b'hash':
+            client.hincrby(user_id, position_id, 1)
     else:
-        return None
+        client.hincrby(user_id, position_id, 1)
+    return {"status": 200}
+
+
+
+def redis_get_from_cart(user_id):
+    values = None
+    if client.exists(user_id):
+        key_type = client.type(user_id)
+        if key_type == b'list':
+            values = client.lrange(user_id, 0, -1)
+        elif key_type == b'hash':
+            values = client.hgetall(user_id)
+            # Decode the bytes objects to strings
+            values = {key.decode('utf-8'): value.decode('utf-8') for key, value in values.items()}
+    else:
+        values = None  # Define the values variable in the else block
+    return values
+
+
+def redis_clear_cart(user_id):
+    if client.exists(user_id):
+        client.delete(user_id)
+    return True
