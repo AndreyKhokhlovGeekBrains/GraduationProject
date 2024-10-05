1ca06c01ee0d253893c74c2de7a7bc0a30d6ba66
Hryharovich Daniil <drink.wtoroy@gmail.com>
2024-10-05 19:59:39 +0300

    Сhange receipt type of 'amount' and 'position_id' vars in cart.py

--- cart/cart.py
+++ cart/cart.py
@@ -18,20 +18,24 @@ async def get_cart(request: Request):
         return templates.TemplateResponse("cart.html", {"request": request, "content": content})
     return RedirectResponse("/login/")
 
-@router.get("/add/{position_id}/{amount}")
-async def add_cart(request: Request, position_id: int, amount: int):
+@router.get("/add/")
+async def add_cart(request: Request):
     token = request.cookies.get("JWT")
     if token:
+        position_id = int(request.query_params.get("position_id"))
+        amount = int(request.query_params.get("amount"))
         decoded_token = decode_token(token)
         status = redis_add_to_cart(user_id=decoded_token.id, position_id=position_id, amount=amount)
         if status["status"] == 200:
             return status.update({"msg": "Position successful added to cart!"})
     return RedirectResponse("/login/")
 
-@router.get("/delete/{position_id}/{amount}")
-async def del_cart(request: Request, position_id: int, amount: int):
+@router.get("/delete/")
+async def del_cart(request: Request):
     token = request.cookies.get("JWT")
     if token:
+        position_id = int(request.query_params.get("position_id"))
+        amount = int(request.query_params.get("amount"))
         decoded_token = decode_token(token)
         status = redis_remove_from_cart(user_id=decoded_token.id, position_id=position_id, amount=amount)
         if status["status"] == 200:
