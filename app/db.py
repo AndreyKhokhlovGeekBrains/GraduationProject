e148ebf1395589c23b08dcdc501ff878100fbcab
DaniilPython <151217635+Daniil2321@users.noreply.github.com>
2024-10-10 21:55:41 +0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -6,12 +6,10 @@ from sqlalchemy.orm import sessionmaker
 # Use environment variable to switch between local and Docker environments
 DATABASE_URL = "postgresql://postgres:68064911@localhost:5432/postgres"
 
-<<<<<<< HEAD
+
 database = Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
-=======
-database = databases.Database(DATABASE_URL)
->>>>>>> ea4a09d2c20a4d9611a5515123ba27b2c23e7a21
+
 
 
 
